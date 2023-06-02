69f1bb1fa82fe20d2fd76265d961011ed3d1e70c
MhmmdSyahputra <putra.cokers@gmail.com>
2023-06-03 01:12:11 +0700

    add react

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function CreateMainWindow() {
     const mainWindow = new BrowserWindow({
         title: 'Electron',
         width: 1000,
-        height: 600
+        height: 600,
     });
 
+    mainWindow.webContents.openDevTools()
+
     const startUrl = url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './app/build/index.html'),
         protocol: 'file'
     })
 
